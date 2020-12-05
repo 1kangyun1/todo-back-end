94e0011b0beb80ea2d6b24738a7b329d43fc326c
Brian Kang <1kangyun1@gmail/com>
2020-12-05 17:24:41 -0500

    fixing database access

--- routes/todoList.js
+++ routes/todoList.js
@@ -2,53 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  //database access
-  const { Client } = require('pg');
-
-  const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
-
-  client.connect();
-
-  client.query('SELECT * FROM todos', (err, res) => {
-    if (err){
-      throw err;
-    }
-    for (let row of res.rows) {
-      console.log(JSON.stringify(row));
-    }
-  })
-  client.end();
-
-  res.json([
-    {
-      "id": 1,
-      "title": "de lectus aut autem",
-      "completed": false
-    },
-    {
-      "id": 2,
-      "title": "quis ut nam facilis et officia qui",
-      "completed": false
-    },
-    {
-      "userId": 1,
-      "id": 3,
-      "title": "fugiat veniam minus",
-      "completed": false
-    },
-    {
-      "userId": 1,
-      "id": 4,
-      "title": "et porro tempora",
-      "completed": true
-    }
-  ]);
+router.get('/', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM todos');
+    const results = { 'results': (result) ? result.rows : null};
+    res.json( results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
 });
 
 module.exports = router;

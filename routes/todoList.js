8b05c393c01eeab2bcb5478bd7d8bfd944a501cd
Brian Kang <1kangyun1@gmail/com>
2020-12-06 15:35:09 -0500

    created toggle function

--- routes/todoList.js
+++ routes/todoList.js
@@ -3,6 +3,26 @@ const { Pool } = require('pg');
 
 var router = express.Router();
 
+router.get('/:id', async(req, res) => {
+  try {
+    const pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    });
+
+    const client = await pool.connect();
+    result = await client.query('SELECT * FROM todos WHERE id = $1;', [req.params.id]);
+    result = await client.query('UPDATE todos SET finished = $1 WHERE id = $2;', [!result.rows.finished, req.params.id]);
+    res.json( result.rows );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
 router.get('/', async(req, res) => {
   try {
     const pool = new Pool({

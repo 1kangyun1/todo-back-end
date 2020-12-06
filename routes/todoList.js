9143681932e0194923a064b94d230e753dfc7a86
Brian Kang <1kangyun1@gmail/com>
2020-12-06 15:50:55 -0500

    fixing toggle function

--- routes/todoList.js
+++ routes/todoList.js
@@ -14,8 +14,8 @@ router.get('/:id', async(req, res) => {
 
     const client = await pool.connect();
     result = await client.query('SELECT * FROM todos WHERE id = $1;', [req.params.id]);
-    res.json( result.rows );
-    //result = await client.query('UPDATE todos SET finished = $1 WHERE id = $2 RETURNING *;', [!result.rows.finished, req.params.id]);
+    res.json( !result.rows.finished );
+    result = await client.query('UPDATE todos SET finished = $1 WHERE id = $2 RETURNING *;', [!result.rows.finished, req.params.id]);
     //res.json( result.rows );
     client.release();
   } catch (err) {

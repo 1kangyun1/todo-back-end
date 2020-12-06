4572eaf323cc98669254187607c40906739f8b3a
Brian Kang <1kangyun1@gmail/com>
2020-12-06 16:24:59 -0500

    fixing check function

--- routes/todoList.js
+++ routes/todoList.js
@@ -14,9 +14,8 @@ router.get('/:id', async(req, res) => {
 
     const client = await pool.connect();
     result = await client.query('SELECT * FROM todos WHERE id = $1;', [req.params.id]);
-    const check = result.rows[0].finished;
-
-    result = await client.query('UPDATE todos SET finished = $1 WHERE id = $2 RETURNING *;', [(check ? false : true), req.params.id]);
+    result = await client.query('UPDATE todos SET finished = $1 WHERE id = $2 RETURNING *;', [!result.rows[0].finished, req.params.id]);
+    
     res.json( result.rows );
     client.release();
   } catch (err) {

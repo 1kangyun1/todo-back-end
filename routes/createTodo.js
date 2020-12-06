cf0eda48b218f23f98ae5575a198bb069bd5af2e
Brian Kang <1kangyun1@gmail/com>
2020-12-06 14:23:57 -0500

    fixed insert and delete function

--- routes/createTodo.js
+++ routes/createTodo.js
@@ -15,7 +15,7 @@ router.post('/', async(req, res) => {
 
     const client = await pool.connect();
     const result = await client.query('INSERT INTO todos VALUES (?, ?, ?) RETURNING *;', 
-                                          [uuid(), req.body.title, req.body.completed]);
+                                          [uuid(), req.body.todo, req.body.completed]);
     res.json( result.rows );
     client.release();
   } catch (err) {
